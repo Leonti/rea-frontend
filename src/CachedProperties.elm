56aa53e18794e04545760ec4cc879c7e5802d26b
Leonti Bielski <leonti.bielski@myob.com>
2017-11-17 13:18:06 +1100

    Start working on results caching

---
+++ src/CachedProperties.elm
@@ -0,0 +1,57 @@
+module CachedProperties exposing (State)
+
+import Data.Session as Session exposing (Session)
+import Data.OnSaleProperty as OnSaleProperty exposing (OnSaleProperty)
+import Data.SoldProperty as SoldProperty exposing (SoldProperty)
+import Request.OnSaleProperty
+import Task exposing (Task)
+import Http
+
+
+type alias State =
+    { onSale : LoadingState (List OnSaleProperty)
+    , sold : LoadingState (List SoldProperty)
+    }
+
+
+type LoadingState a
+    = NotLoaded
+    | Loading
+    | Loaded a
+    | Failed String
+
+
+type Msg
+    = OnSaleResult (Result Http.Error (List OnSaleProperty))
+    | SoldResult (Result Http.Error (List SoldProperty))
+
+
+init : Session -> ( State, Cmd Msg )
+init session =
+    let
+        loadOnSaleProperties =
+            Request.OnSaleProperty.all session.maybeAuthToken
+                |> Http.toTask
+
+        state =
+            { onSale = Loading
+            , sold = Loading
+            }
+    in
+        ( state, Task.attempt OnSaleResult loadOnSaleProperties )
+
+
+update : State -> Msg -> State
+update state msg =
+    case msg of
+        OnSaleResult (Ok onSale) ->
+            { state | onSale = Loaded onSale }
+
+        OnSaleResult (Err _) ->
+            { state | onSale = Failed "Failed to load on sale properties" }
+
+        SoldResult (Ok sold) ->
+            { state | sold = Loaded sold }
+
+        SoldResult (Err _) ->
+            { state | sold = Failed "Failed to load sold properties" }
